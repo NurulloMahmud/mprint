dd73fda6ff829701687f0bb7657d251023d8d24f
NurullohMahmud <nurullomahmud@gmail.com>
2024-04-27 15:41:06 -0500

    main models

--- main/models.py
+++ main/models.py
@@ -1,20 +1,134 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class Paper(models.Model):
+
+class Branch(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Product(models.Model):
     name = models.CharField(max_length=100)
     weight = models.FloatField()
     cost = models.DecimalField(decimal_places=2, max_digits=10)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Inventory(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.product.name
+
+
+class Size(models.Model):
+    name = models.CharField(max_length=100)
+    paper = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=13, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Order(models.Model):
+    date = models.DateField(auto_now_add=True)
+    name = models.CharField(max_length=500)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    total_price = models.DecimalField(decimal_places=2, max_digits=10)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+    agent = models.ForeignKey(User, on_delete=models.CASCADE)
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=100)
     price = models.DecimalField(decimal_places=2, max_digits=10)
 
     def __str__(self) -> str:
         return self.name
 
 
-class Size(models.Model):
-    name = models.CharField(max_length=100)
-    paper = models.ForeignKey(Paper, on_delete=models.CASCADE)
+class InventoryOrder(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.DecimalField(decimal_places=2, max_digits=10)
 
     def __str__(self) -> str:
-        return self.name
+        return self.order.name
+
+
+class ServiceOrder(models.Model):
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self) -> str:
+        return self.service.name
+
+
+class Purchase(models.Model):
+    date = models.DateField(auto_now_add=True)
+    item = models.CharField(max_length=200)
+    unit_price = models.DecimalField(decimal_places=2, max_digits=10, null=True, blank=True)
+    quantity = models.PositiveIntegerField(null=True, blank=True)
+    total_price = models.DecimalField(decimal_places=2, max_digits=10, null=True, blank=True)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.item
+
+
+class CustomerDebt(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    amount = models.DecimalField(decimal_places=2, max_digits=10)
+    last_update = models.DateField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.customer.name
+
+
+class OrderPayment(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    amount = models.DecimalField(decimal_places=2, max_digits=10)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.order.name
+
+
+class Debt(models.Model):
+    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
+    amount = models.DecimalField(decimal_places=2, max_digits=10)
+
+    def __str__(self) -> str:
+        return self.purchase.item
 
--- users/models.py
+++ users/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from main.models import Branch
+
 
 class Role(models.Model):
     name = models.CharField(max_length=100)
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE, null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
