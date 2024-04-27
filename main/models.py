dae001b4f3d0a692820f38b363116904463b4fa4
NurullohMahmud <nurullomahmud@gmail.com>
2024-04-27 14:12:05 -0500

    main model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Paper(models.Model):
+    name = models.CharField(max_length=100)
+    weight = models.FloatField()
+    cost = models.DecimalField(decimal_places=2, max_digits=10)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Size(models.Model):
+    name = models.CharField(max_length=100)
+    paper = models.ForeignKey(Paper, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+
