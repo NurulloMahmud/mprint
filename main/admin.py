b2adda29b7e0997da9a067c74eaf1c5a1198ea84
NurullohMahmud <nurullomahmud@gmail.com>
2024-04-27 16:01:23 -0500

    main admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Branch, Status, Product, Inventory, Size, Customer, Order, Service, InventoryOrder, ServiceOrder, Purchase, CustomerDebt, OrderPayment, Debt
 
 # Register your models here.
+admin.site.register(Branch)
+admin.site.register(Status)
+admin.site.register(Product)
+admin.site.register(Inventory)
+admin.site.register(Size)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(Service)
+admin.site.register(InventoryOrder)
+admin.site.register(ServiceOrder)
+admin.site.register(Purchase)
+admin.site.register(CustomerDebt)
+admin.site.register(OrderPayment)
+admin.site.register(Debt)
