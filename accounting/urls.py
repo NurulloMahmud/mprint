6df3e1e3604df453dcd0b750ddbc6103b0882306
nurulloh <nurullomahmud@gmail.com>
2024-05-20 16:49:18 +0500

    admin

--- accounting/urls.py
+++ accounting/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from .views import ExpenseCategoryViewSet, ExpensesModelViewset
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'expense_category', ExpenseCategoryViewSet, basename='expense_category')
+router.register(r'expenses', ExpensesModelViewset, basename='expenses')
+
 
 
 urlpatterns = [
-
+    path('', include(router.urls))
 ]
