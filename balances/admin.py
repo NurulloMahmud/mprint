90e8d7b7395bcb47100ab354d6b8ec4d44059384
NurulloMahmud <nurullomahmud@gmail.com>
2024-10-01 17:49:16 +0500

    total

--- balances/admin.py
+++ balances/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Stakeholder, BalanceSheet, Expenses
 
-# Register your models here.
+
+admin.site.register(Stakeholder)
+admin.site.register(BalanceSheet)
+admin.site.register(Expenses)   
